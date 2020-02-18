7e159261ffdd41423cf1c0ffa0fd89ab1f3e06c2
Huang Qi <huangqi.0@bytedance.com>
2020-02-19 00:47:48 +0800

    update(scroller)

--- src/revas/components/ScrollView.ts
+++ src/revas/components/ScrollView.ts
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { NodeProps, RevasTouchEvent, Frame } from '../core/Node'
-import { clamp } from '../core/utils'
+import { NodeProps, Frame } from '../core/Node'
+import Scroller from '../common/Scroller'
 
 export type ScrollViewProps = {
 
@@ -32,7 +32,7 @@ export default class ScrollView extends React.Component<ScrollViewProps> {
 
   private _checkLayout = () => {
     const max = this._contentHeight - this._height
-    if (max !== this._scroller.max) {
+    if (max > 0 && max !== this._scroller.max) {
       this._scroller.max = max
     }
   }
@@ -57,67 +57,4 @@ export default class ScrollView extends React.Component<ScrollViewProps> {
       })
     )
   }
-}
-
-class Scroller {
-  top = 0
-
-  constructor(
-    private handler: (top: number) => any,
-    public max = -1
-  ) { }
-
-  private _lastY = -1
-  private _lastTimestamp = 0
-  private _v = 0
-  private _tid = 0
-
-  touchStart = (e: RevasTouchEvent) => {
-    this._tid = +Object.keys(e.touches)[0]
-    this._lastTimestamp = e.timestamp
-    this._lastY = e.touches[this._tid].y
-  }
-
-  touchMove = (e: RevasTouchEvent) => {
-    const { y } = e.touches[this._tid]
-    const moveY = this._lastY - y
-    const duration = e.timestamp - this._lastTimestamp
-    this._v = moveY / duration
-    this._lastTimestamp = e.timestamp
-    this._lastY = y
-    this.change(moveY)
-  }
-
-  touchEnd = (e: RevasTouchEvent) => {
-    this._lastTimestamp = Date.now()
-    this._lastY = -1
-    requestAnimationFrame(this.afterEnd)
-  }
-
-  afterEnd = () => {
-    if (this._lastY < 0 && Math.abs(this._v) > 0.1) {
-      const timestamp = Date.now()
-      const duration = timestamp - this._lastTimestamp
-      this._v = friction(this._v, duration)
-      const moveY = this._v * duration
-      this._lastTimestamp = timestamp
-      this.change(moveY)
-      requestAnimationFrame(this.afterEnd)
-    }
-  }
-
-  change(top: number) {
-    const _top = clamp(this.top + top, 0, this.max > 0 ? this.max : 0)
-    // check validate
-    if (_top !== this.top) {
-      this.top = _top
-      this.handler(this.top)
-    } else if (this._lastY < 0) {
-      this._v = 0
-    }
-  }
-}
-
-function friction(v: number, duration: number) {
-  return v - (duration * 0.005 * v)
 }
