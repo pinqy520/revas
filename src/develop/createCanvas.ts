0a4c975a4a2c71f817bc96d201d3bb040ca8c4a4
Huang Qi <huangqi.0@bytedance.com>
2020-05-05 19:04:26 +0800

    feat: update

--- src/develop/createCanvas.ts
+++ src/develop/createCanvas.ts
@@ -1,8 +1,8 @@
 export default function () {
   const canvas = document.createElement('canvas');
   const scale = window.devicePixelRatio;
-  const width = Math.min(window.innerWidth, 420);
-  const height = Math.min(window.innerHeight, 840);
+  const width = Math.min(window.innerWidth, 450);
+  const height = Math.min(window.innerHeight, 900);
   canvas.width = width * scale;
   canvas.height = height * scale;
   canvas.setAttribute('style', `width: ${width}px; height: ${height}px;`);
